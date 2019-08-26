99542db32c63439227b8241510f18c0921c1081c
Carlos <50813920+xXNachoGSXx@users.noreply.github.com>
2019-08-26 16:46:24 -0600

    Portafolio
    Portafolio para Electiva Web

---
+++ js/conversorfun.js
@@ -0,0 +1,16 @@
+
+
+
+
+function limpiar() {
+    document.getElementById("monedaBrasil").value = "";
+    document.getElementById("monedaChina").value = "";
+    document.getElementById("monedaJapon").value = "";
+    document.getElementById("monedaRusia").value = "";
+    document.getElementById("monedaSafrica").value = "";
+    document.getElementById("monedaCr").value = "";
+    document.getElementById("monedaAlemana").value = "";
+    document.getElementById("monedaUk").value = "";
+    document.getElementById("monedaUs").value = "";
+    document.getElementById("monedaUru").value = ""; 
+}
